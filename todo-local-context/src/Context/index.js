d0278fc065b4b5ed6e36422ff29f7e4ac73d555c
sahilgadekar1507 <sahilgadekar15@gmail.com>
2025-08-05 19:02:54 +0530

    done with the context and local storage and created the todo app

---
+++ todo-local-context/src/Context/index.js
@@ -0,0 +1,5 @@
+// import {TodoContext, TodoProvider, useTodo} from "./TodoContext"
+
+import { TodoContext, TodoProvider, useTodo } from "./TodoContext";
+
+export { TodoContext, TodoProvider, useTodo };
